ed325c38d621bb23d1b1b169510a23a68aa0e8af
Alessandro Cuppari <alessandro.cuppari@broadridge.com>
2025-12-18 12:17:04 +0000

    todo-server registered

--- todo_server.exs
+++ todo_server.exs
@@ -1,6 +1,9 @@
 defmodule TodoServer do
   def start do
-    spawn(fn -> loop(TodoList.new()) end)
+    spawn(fn ->
+      Process.register(self(), :todo_server)
+      loop(TodoList.new())
+    end)
   end
 
   defp loop(todo_list) do
@@ -12,8 +15,8 @@ defmodule TodoServer do
     loop(new_todo_list)
   end
 
-  def entries(todo_server, date) do
-    send(todo_server, {:entries, self(), date})
+  def entries(date) do
+    send(:todo_server, {:entries, self(), date})
 
     receive do
       {:todo_entries, entries} -> entries
@@ -22,16 +25,16 @@ defmodule TodoServer do
     end
   end
 
-  def add_entry(todo_server, new_entry) do
-    send(todo_server, {:add_entry, new_entry})
+  def add_entry(new_entry) do
+    send(:todo_server,{:add_entry, new_entry})
   end
 
-  def delete_entry(todo_server, entry_id) do
-    send(todo_server, {:delete_entry, entry_id})
+  def delete_entry(entry_id) do
+    send(:todo_server,{:delete_entry, entry_id})
   end
 
-  def update_entry(todo_server, entry_id, updater_fun) do
-    send(todo_server, {:update_entry, entry_id, updater_fun})
+  def update_entry(entry_id, updater_fun) do
+    send(:todo_server,{:update_entry, entry_id, updater_fun})
   end
 
   defp process_message(todo_list, {:entries, caller, date}) do
