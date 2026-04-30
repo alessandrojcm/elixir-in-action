67dacc33d292c34b2f3a50d5ae6de152f56fd6d7
Alessandro Cuppari <alessandro.cuppari@toasttab.com>
2026-04-30 17:21:23 +0100

    server process sync

---
+++ server_process.ex
@@ -0,0 +1,48 @@
+defmodule ServerProcess do
+  def start(callback_module) do
+    spawn(fn ->
+      initial_state = callback_module.init()
+      loop(callback_module, initial_state)
+    end)
+  end
+
+  def loop(callback_module, current_state) do
+    receive do
+      {:call, request, caller} ->
+        {response, new_state} = callback_module.handle_call(request, current_state)
+        send(caller, {:response, response})
+        loop(callback_module, new_state)
+    end
+  end
+
+  def call(server_pid, request) do
+    send(server_pid, {:call, request, self()})
+
+    receive do
+      {:response, response} ->
+        response
+    end
+  end
+end
+
+defmodule KeyValueStore do
+  def init do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def get(pid, key, value) do
+    ServerProces.call(pid, {:put, key, value})
+  end
+
+  def put(pid, key) do
+    ServerProcess.call(pid, {:get, key})
+  end
+
+  def handle_call({:put, key, value}, state) do
+    {:ok, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, state) do
+    {Map.get(state, key), state}
+  end
+end
